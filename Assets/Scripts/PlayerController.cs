0b3ae4ace655d4e2e0eab3534b6d2c82f5ec25c4
funnbot <dillonbayless1@gmail.com>
2018-06-03 09:09:15 -0700

    Rooms

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
@@ -16,6 +18,9 @@ public class PlayerController : MonoBehaviour {
     Vector3 input;
     Vector2 mouse;
 
+    public Text info;
+    float textTimer;
+
     void Start() {
         rb = GetComponent<Rigidbody>();
         mouse = new Vector2(transform.localEulerAngles.x, transform.localEulerAngles.y);
@@ -28,11 +33,16 @@ public class PlayerController : MonoBehaviour {
         Vector3 angles = transform.localEulerAngles;
         angles.y = mouse.y;
         transform.localEulerAngles = angles;
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            SceneManager.LoadScene("Game");
+        }
     }
 
     void FixedUpdate() {
         Vector3 pos = (input.x * rb.transform.right) + (input.z * rb.transform.forward);
-        rb.MovePosition(rb.transform.position + pos * speed);
+        transform.position += pos * speed;
+        // rb.MovePosition(transform.position + pos * speed);
     }
 
     void LateUpdate() {
@@ -42,5 +52,11 @@ public class PlayerController : MonoBehaviour {
         cam.transform.position = transform.position;
         angles.y = transform.localEulerAngles.y;
         cam.transform.localEulerAngles = angles;
+    }
+
+    bool talkWithNan;
+
+    void SetText(string t) {
+        
     }
 }
