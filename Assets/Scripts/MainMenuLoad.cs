59a54d76f59773d5745d5b6c4dfb494de40c0208
funnbot <dillonbayless1@gmail.com>
2018-06-03 17:39:49 -0700

    Game

--- Assets/Scripts/MainMenuLoad.cs
+++ Assets/Scripts/MainMenuLoad.cs
@@ -7,12 +7,7 @@ public class MainMenuLoad : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-
+        Screen.SetResolution(1920, 1080, FullScreenMode.ExclusiveFullScreen);
     }
 
     public void Enter() {
