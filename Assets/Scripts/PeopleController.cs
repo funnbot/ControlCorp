435222d394874adb494dfa70bc95c72dff6f8ca8
funnbot <dillonbayless1@gmail.com>
2018-06-03 17:22:52 -0700

    Done

---
+++ Assets/Scripts/PeopleController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class PeopleController : MonoBehaviour {
+
+    public float wanderRadius;
+    public float wanderTimer;
+
+    private Transform target;
+    private NavMeshAgent agent;
+    private float timer;
+
+    PlayerController player;
+
+    // Use this for initialization
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        timer = 0;
+
+        agent.enabled = false;
+        agent.enabled = true;
+
+        player = PlayerController.Instance;
+    }
+
+    bool pressed;
+
+    // Update is called once per frame
+    void Update() {
+        timer += Time.deltaTime;
+
+        if (player.buttonDown) {
+            if (!pressed) {
+                Debug.Log("pressed");
+                pressed = true;
+                IntoMC();
+            }
+
+        } else pressed = false;
+
+        if (timer >= wanderTimer) {
+            Vector3 newPos = randomPos();
+            Debug.Log(newPos);
+            agent.SetDestination(newPos);
+            timer = Random.Range(-3, 5);
+        }
+    }
+
+    Vector3 randomPos() {
+        float x = Random.Range(-7, 8);
+        float y = Random.Range(-7, 8);
+        return new Vector3(x, 0.5f, y) + transform.parent.position;
+    }
+
+    [ContextMenu("Into MC")]
+    void IntoMC() {
+        timer = -5;
+        agent.SetDestination(transform.parent.position + new Vector3(0.25f, 0.5f, 6.25f));
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,17 +13,30 @@ public class PlayerController : MonoBehaviour {
     public Vector2 limitX;
     [Range(1, 10)]
     public float lookSpeed;
+    public Transform button;
+    public Material wallMaterial;
 
     Rigidbody rb;
     Vector3 input;
     Vector2 mouse;
+    Vector3 offset;
+
+    RaycastHit hit;
+    bool lookingAt;
 
     public Text info;
+    public Text speech;
+    bool textSet;
     float textTimer;
+
+    public bool buttonDown;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
         mouse = new Vector2(transform.localEulerAngles.x, transform.localEulerAngles.y);
+        info.text = "";
+        speech.text = "";
+        offset = transform.position - cam.transform.position;
     }
 
     void Update() {
@@ -34,9 +47,26 @@ public class PlayerController : MonoBehaviour {
         angles.y = mouse.y;
         transform.localEulerAngles = angles;
 
+        RayCast();
+        if (lookingAt) ToolTip();
+        else SetInfo("");
+
         if (Input.GetKeyDown(KeyCode.R)) {
             SceneManager.LoadScene("Game");
         }
+
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (lookingAt) Interact();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+
+        if (textTimer <= 0f) {
+            speech.text = "";
+        } else textTimer -= Time.deltaTime;
     }
 
     void FixedUpdate() {
@@ -49,18 +79,67 @@ public class PlayerController : MonoBehaviour {
         Vector3 angles = cam.transform.localEulerAngles;
         mouse.x = Mathf.Clamp(mouse.x, limitX.x, limitX.y);
         angles.x = mouse.x;
-        cam.transform.position = transform.position;
+        cam.transform.position = transform.position - offset;
         angles.y = transform.localEulerAngles.y;
         cam.transform.localEulerAngles = angles;
     }
 
     bool talkWithNan;
 
-    void SetText(string t) {
-        
+    void ToolTip() {
+        GameObject go = hit.collider.gameObject;
+        if (go.CompareTag("Nantucket")) {
+            SetInfo("Press E to talk to the Nantucket Representitive");
+        }
+        if (go.CompareTag("Paul")) {
+            SetInfo("Press E to talk to Paul Ryan");
+        }
+        if (go.CompareTag("RedButton")) {
+            SetInfo("Press E to control their appetite");
+        }
     }
 
-    void TimedText(string t) {
-        
+    void Interact() {
+        GameObject go = hit.collider.gameObject;
+        if (go.CompareTag("Nantucket")) {
+            SetSpeech("\"Paul Ryan will be reforming taxes soon, ensure that our needs are met in the new bill.\"");
+            talkWithNan = true;
+        }
+        if (go.CompareTag("Paul")) {
+            if (talkWithNan) {
+                SetSpeech("\"Wow, your funraiser brought me over 10 million dollars, now what were those changes you wanted me to make again?\"");
+            } else SetSpeech("Do I know you?");
+        }
+        if (go.CompareTag("RedButton")) {
+            if (!buttonDown) {
+                button.Translate(0f, 0f, -0.1f);
+                wallMaterial.color = Color.red;
+            } else {
+                button.Translate(0f, 0f, 0.1f);
+                wallMaterial.color = Color.white;
+            }
+            buttonDown = !buttonDown;
+        }
+    }
+
+    void RayCast() {
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        lookingAt = Physics.Raycast(ray, out hit, 2.5f);
+    }
+
+    void SetSpeech(string t) {
+        textTimer = 5;
+        speech.text = t;
+    }
+
+    void SetInfo(string t) {
+        info.text = t;
+    }
+
+
+
+    public static PlayerController Instance;
+    void Awake() {
+        Instance = this;
     }
 }
